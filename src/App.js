dce7e4b03970d9341bee751e396e91c0d7915bed
sakshi-124 <vaidyasakshi434@gmail.com>
2023-04-09 09:24:00 -0300

    firstpagechanegs done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  useLocation
 } from "react-router-dom";
 import Login from './Pages/Login';
 import Register from './Pages/Register';
@@ -14,23 +15,49 @@ import AvailableRides from './Pages/AvailableRides';
 import ConfirmRides from './Pages/ConfirmRides';
 
 function App() {
-  
+  const location = useLocation(); // get the current location of the page
+  const showNavbar = location.pathname !== "/"; // check if the current path is not "/" (i.e. Login page)
   return (
     <div>
-      <RideShareNav/>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Login/>} />
+      {showNavbar && <RideShareNav />}
+      <Routes>
+      <Route path='/' element={<Login/>} />
           <Route path='/register' element={<Register/>} />
           <Route path='/confirmUser' element={<Confirm/>} />
           <Route path ='/postRide' element = { <PostRide/>}/>
           <Route path ='/availableRides' element = { <AvailableRides/>}/>
           <Route path ='/rideRequests' element = { <ConfirmRides/>}/>
-        </Routes>
-      </Router>
+      </Routes>
     </div>
-
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+// function App() {
+  
+//   return (
+//     <div>
+//       <RideShareNav/>
+//       <Router>
+//         <Routes>
+//           <Route path='/' element={<Login/>} />
+//           <Route path='/register' element={<Register/>} />
+//           <Route path='/confirmUser' element={<Confirm/>} />
+//           <Route path ='/postRide' element = { <PostRide/>}/>
+//           <Route path ='/availableRides' element = { <AvailableRides/>}/>
+//           <Route path ='/rideRequests' element = { <ConfirmRides/>}/>
+//         </Routes>
+//       </Router>
+//     </div>
+
+//   );
+// }
+
+export default AppWrapper;
